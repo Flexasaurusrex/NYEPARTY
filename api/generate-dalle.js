fdf9fd9dd1acb10bc750328b05f9e957797bbfba
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-12-27 01:59:11 -0800

    Update generate-dalle.js

--- api/generate-dalle.js
+++ api/generate-dalle.js
@@ -1,5 +1,3 @@
-const fetch = require('node-fetch');
-
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
